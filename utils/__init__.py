cc86efdbe5badc3b0a5a0be2c0b5414eb3dbd249
Sören Erichsen <soeren.erichsen@ibeo-as.com>
2020-09-17 17:11:33 +0200

    Adds utility folder with file system script. This script contains file system util methods.

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+ 
+File:
+    __init__.py
+ 
+Authors: umla, soe
+Date:
+    16.09.20
+ 
+Copyright:
+    (c) Ibeo Automotive Systems GmbH, Hamburg, Germany
+"""
---
+++ utils/filesystem.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+ 
+File:
+    filesystem.py
+ 
+Authors: soe
+Date:
+    17.09.20
+
+"""
+
+import os
+
+
+def create_folder(path: str):
+    """
+    Creates a new folder under path if it does not already exist
+    :param
+        path: name/path to folder
+    :return:
+        -
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
